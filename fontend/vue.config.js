d80624032159965f89c2de455aa1823018aafc40
“Nguyen_Duc_Canh” <nduccanh2003@gmail.com>
2025-03-19 10:11:02 +0700

    cập nhật cấu hình vuejs

--- fontend/vue.config.js
+++ fontend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+//Cấu hình proxy để tráng lỗi CORS khi gọi API
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {  // Mọi request bắt đầu bằng /api sẽ được chuyển đến backend
+        target: 'http://localhost:3000', // URL của backend
+        changeOrigin: true, // Cần thiết cho virtual hosted sites
+        // pathRewrite: { // (Tùy chọn) Nếu bạn muốn thay đổi đường dẫn
+        //   '^/api': '' // Ví dụ: Xóa /api khỏi đường dẫn
+        // }
+      }
+    }
+  }
 })
+
