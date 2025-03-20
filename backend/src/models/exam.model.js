2188a27283e99528a535e9a0f287d11fec9ee012
Tran Lieu Huy Khanh <lieuhuykhanh@gmail.com>
2025-03-20 09:14:04 +0700

    Bốc đề tự động

--- backend/src/models/exam.model.js
+++ backend/src/models/exam.model.js
@@ -1,44 +1,59 @@
 // src/models/exam.model.js
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
-const ExamCategory = require('./exam_category.model');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+const ExamCategory = require("./exam_category.model");
 
-const Exam = sequelize.define('Exam', {
+const Exam = sequelize.define(
+  "Exam",
+  {
     exam_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     exam_name: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    easy_question_count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    medium_question_count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    hard_question_count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
     },
     description: {
-        type: DataTypes.TEXT,
+      type: DataTypes.TEXT,
     },
     category_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: ExamCategory,
-            key: 'category_id',
-        },
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: ExamCategory,
+        key: "category_id",
+      },
     },
     created_at: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW, // Sử dụng DataTypes.NOW để tự động lấy thời gian hiện tại
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW, // Sử dụng DataTypes.NOW để tự động lấy thời gian hiện tại
     },
     updated_at: {
-        type: DataTypes.DATE,
+      type: DataTypes.DATE,
     },
-}, {
-    tableName: 'exams',
+  },
+  {
+    tableName: "exams",
     timestamps: true, // Bật timestamps (createdAt, updatedAt)
-    updatedAt: 'updated_at', // Đặt tên tùy chỉnh cho trường updatedAt
-    createdAt: 'created_at', // Đặt tên tùy chỉnh cho trường createdAt
+    updatedAt: "updated_at", // Đặt tên tùy chỉnh cho trường updatedAt
+    createdAt: "created_at", // Đặt tên tùy chỉnh cho trường createdAt
+  }
+);
 
-});
-
-Exam.belongsTo(ExamCategory, { foreignKey: 'category_id', as: 'category' });
+Exam.belongsTo(ExamCategory, { foreignKey: "category_id", as: "category" });
 
 module.exports = Exam;
