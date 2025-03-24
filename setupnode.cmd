9421d68fcdfac08363e9e02a7dffe3321b36877a
lecongchuc <0l14l301@gmail.com>
2025-03-24 16:24:33 +0700

    Add command to run cmd file in right folder

--- setupnode.cmd
+++ setupnode.cmd
@@ -1,4 +1,5 @@
 @echo off
 echo - Đang cài đặt các thư viện Node.js...
+cd /d %~dp0
 powerShell "echo '+ Cài đặt backend................................................................................'; cd backend; npm install; cd ..; echo ''; echo ''; echo '+ Cài đặt fontend................................................................................'; cd fontend; npm install;"
 echo - Cài đặt hoàn tất.
