cccde19017b8bea694be84cb2287eb375b3a2c0c
lecongchuc <0l14l301@gmail.com>
2025-03-24 12:38:19 +0700

    Create AutoSetup script

---
+++ setupnode.cmd
@@ -0,0 +1,4 @@
+@echo off
+echo - Đang cài đặt các thư viện Node.js...
+powerShell "echo '+ Cài đặt backend................................................................................'; cd backend; npm install; cd ..; echo ''; echo ''; echo '+ Cài đặt fontend................................................................................'; cd fontend; npm install;"
+echo - Cài đặt hoàn tất.
